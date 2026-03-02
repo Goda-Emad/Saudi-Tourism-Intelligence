d1800fee9368ea3ccafd7351b17ab6a9334fbf40
goda emad <goda.emade2001@gmail.com>
2026-03-02 16:15:33 +0200

    Update __init__.py

--- dashboard/utils/__init__.py
+++ dashboard/utils/__init__.py
@@ -1,1 +1,72 @@
+# utils/__init__.py
+"""
+Saudi Tourism Intelligence - Utilities Package
+هذا الملف يجعل المجلد package قابلاً للاستيراد
+"""
 
+from .kpis import (
+    load_all_data,
+    calculate_kpis,
+    format_number,
+    get_yoy_growth,
+    calculate_growth_rate,
+    get_peak_month,
+    get_seasonal_factors
+)
+
+from .charts import (
+    create_trend_chart,
+    create_comparison_chart,
+    create_heatmap,
+    create_pie_chart,
+    create_radar_chart,
+    create_bar_chart,
+    create_line_chart,
+    create_area_chart,
+    create_forecast_chart,
+    create_carbon_chart,
+    create_segment_chart
+)
+
+from .data_loader import (
+    load_tourist_data,
+    load_spending_data,
+    load_overnight_data,
+    load_carbon_data,
+    load_all_datasets,
+    get_data_summary,
+    validate_data
+)
+
+__all__ = [
+    # From kpis
+    'load_all_data',
+    'calculate_kpis',
+    'format_number',
+    'get_yoy_growth',
+    'calculate_growth_rate',
+    'get_peak_month',
+    'get_seasonal_factors',
+    
+    # From charts
+    'create_trend_chart',
+    'create_comparison_chart',
+    'create_heatmap',
+    'create_pie_chart',
+    'create_radar_chart',
+    'create_bar_chart',
+    'create_line_chart',
+    'create_area_chart',
+    'create_forecast_chart',
+    'create_carbon_chart',
+    'create_segment_chart',
+    
+    # From data_loader
+    'load_tourist_data',
+    'load_spending_data',
+    'load_overnight_data',
+    'load_carbon_data',
+    'load_all_datasets',
+    'get_data_summary',
+    'validate_data'
+]
